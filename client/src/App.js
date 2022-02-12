47fa8caf8b824020499b45b42409ce0c8eac7732
troymeeker <troymeeker1@gmail.com>
2022-02-12 13:35:22 -0800

    done

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import {useEffect} from "react";
+import './App.css';
+
+function App() {
+
+  useEffect(()=>{
+    fetch("/movies")
+      .then((resp)=>resp.json())
+      .then((movies)=>console.log(movies));
+  },[])
+
+  return (
+   <h1>Hello  from React!</h1>
+  );
+}
+
+export default App;
